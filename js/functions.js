90a3c8a7f8b74d6ffd220168c7b1b6fb586c0396
Abner Fernandes <tqi_afernandes@uolinc.com>
2022-02-03 23:08:09 -0300

    Final Certs Page

---
+++ js/functions.js
@@ -0,0 +1,34 @@
+window.onload = function(){
+  const elements = ['java','agile','bd','events','devops','css','javascript','arquitecture','git','outros','node','python','react'];
+
+  elements.forEach((item, i) => {
+    document.getElementById(item).style.display = 'none';
+    document.getElementById(item+'-visibility').style.display = 'none';
+  });
+
+
+};
+
+function hideElement(elementName) {
+  var element = document.getElementById(elementName);
+
+  if (element.style.display === "none") {
+    element.style.display = 'block';
+  } else {
+    element.style.display = 'none';
+  }
+}
+
+function takeVisibility(showName, hideName) {
+
+  var showElement = document.getElementById(showName);
+  var hideElement = document.getElementById(hideName);
+
+  if (showElement.style.display === "none") {
+    hideElement.style.display = 'none';
+    showElement.style.display = 'block';
+  } else {
+    hideElement.style.display = 'block';
+    showElement.style.display = 'none';
+  }
+}
